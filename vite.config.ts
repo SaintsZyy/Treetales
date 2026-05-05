32235e751db7b32419ba7980360220656352845d
SaintsZyy <Muhammadfauzia01@gmail.com>
2026-05-05 23:08:59 +0700

    Configure Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
+import { nitro } from "nitro/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig, type PluginOption } from "vite";
+import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ command }) => {
-  const plugins: Array<PluginOption> = [tanstackStart(), react(), tailwindcss(), tsConfigPaths()];
-
-  if (command === "build") {
-    plugins.push(cloudflare());
-  }
-
-  return {
-    plugins,
-  };
+export default defineConfig({
+  plugins: [
+    tanstackStart(),
+    nitro({ preset: "vercel" }),
+    react(),
+    tailwindcss(),
+    tsConfigPaths(),
+  ],
 });
